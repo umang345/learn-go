3cfede96c09fbeedea963f3c8851b45873af9981
Umang <umangagarwal345@gmail.com>
2023-08-25 12:15:13 +0530

    Dockerize go app

---
+++ go-docker-test/go.mod
@@ -0,0 +1,3 @@
+module github.com/umang345/go-docker-test
+
+go 1.19
---
+++ go-docker-test/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"html"
+)
+
+func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter,r *http.Request){
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+
+	http.HandleFunc("/hi", func(w http.ResponseWriter,r *http.Request){
+		fmt.Fprintf(w,"Hi")
+	})
+
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
